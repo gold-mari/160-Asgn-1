ae35f4458c1604f374e08db11ffb998650693ca2
Dylan Mahler <dmahler@ucsc.edu>
2024-04-15 16:07:43 -0700

    Part 3

--- main/HelloPoint1.js
+++ main/HelloPoint1.js
@@ -1,4 +1,9 @@
 // HelloPoint1.js (c) 2012 matsuda
+
+// ================================================================
+// Global variables
+// ================================================================
+
 // Vertex shader program
 var VSHADER_SOURCE = `
     attribute vec4 a_Position;
@@ -16,42 +21,28 @@ var FSHADER_SOURCE = `
         gl_FragColor = u_FragColor;
     }`;
 
+// Globals
+let canvas;
+let gl;
+let a_Position;
+let a_PointSize;
+let u_FragColor;
+let g_points = []; // The array to store the positions of points
+let g_colors = []; // The array to store the colors of a points
+
+// ================================================================
+// Main
+// ================================================================
+
 function main() {
-    // Retrieve <canvas> element
-    var canvas = document.getElementById('webgl');
-
-    // Get the rendering context for WebGL
-    var gl = getWebGLContext(canvas);
-    if (!gl) {
-        console.log('Failed to get the rendering context for WebGL');
-        return;
-    }
-
-    // Initialize shaders
-    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('Failed to intialize shaders.');
-        return;
-    }
-
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-
-    // Get the storage location of u_FragColor variable
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-
-    if (a_Position < 0 || a_PointSize < 0 || !u_FragColor) {
-        if (a_Position < 0) console.log('Failed to get the storage location of a_Position');
-        if (a_PointSize < 0) console.log('Failed to get the storage location of a_PointSize');
-        if (!u_FragColor) console.log('Failed to get u_FragColor variable');
-        return;
-    }
-
-    // Pass vertex position to attribute variable
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-    gl.vertexAttrib1f(a_PointSize, 10.0);
+    
+    // Set up canvas and gl variables
+    setUpWebGL();
+    // Set up GLSL shader programs and connect GLSL variables
+    connectVariablesToGLSL();
 
     // Register function (event handler) to be called on a mouse press
-    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position, u_FragColor); };
+    canvas.onmousedown = click;
 
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -60,16 +51,56 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = []; // The array for a mouse press
-var g_colors = []; // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
-    var x = ev.clientX; // x coordinate of a mouse pointer
-    var y = ev.clientY; // y coordinate of a mouse pointer
-    var rect = ev.target.getBoundingClientRect();
+// ================================================================
+// Initializers
+// ================================================================
 
-    // Transform from client space to WebGL canvas space
-    x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
-    y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+function setUpWebGL() {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+}
+
+function connectVariablesToGLSL() {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+
+    // Get the storage location of u_FragColor variable
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+
+    // Check that all variables exist
+    if (a_Position < 0 || a_PointSize < 0 || !u_FragColor) {
+        if (a_Position < 0) console.log('Failed to get the storage location of a_Position');
+        if (a_PointSize < 0) console.log('Failed to get the storage location of a_PointSize');
+        if (!u_FragColor) console.log('Failed to get u_FragColor variable');
+        return;
+    }
+
+    // Provide default values
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    gl.vertexAttrib1f(a_PointSize, 10.0);
+}
+
+// ================================================================
+// Event callback methods
+// ================================================================
+
+function click(ev) {
+    // Extract the event click and convert to WebGL canvas space
+    let [x, y] = coordinatesEventToGLSpace(ev);
+
     // Store the coordinates to g_points array
     g_points.push([x, y]);
 
@@ -82,6 +113,27 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
         g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
     }
 
+    // Draw every shape that's supposed to be on the canvas.
+    renderAllShapes();
+}
+
+// ================================================================
+// Render methods
+// ================================================================
+
+function coordinatesEventToGLSpace(ev) {
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+
+    // Transform from client space to WebGL canvas space
+    x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
+    y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+
+    return [x, y];
+}
+
+function renderAllShapes() {
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
